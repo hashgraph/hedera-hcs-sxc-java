d49c5f17a70c774b1d56dde2d37dcb47dc25665e
Greg Scullard <gregscullard@hedera.com>
2019-10-24 12:31:35 +0200

    Adding config.yaml for libarry config separate from .env variables

--- src/main/java/com/hedera/hcslib/config/Config.java
+++ src/main/java/com/hedera/hcslib/config/Config.java
@@ -1,65 +1,16 @@
 package com.hedera.hcslib.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.hedera.hashgraph.sdk.account.AccountId;
-import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
-
-import io.github.cdimascio.dotenv.Dotenv;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Config {
+    private List<Node> nodes = new ArrayList<Node>();
     
-    /** 
-     * Manages the readonly configuration data 
-     */
+    public List<Node> getNodes() {
+        return this.nodes;
+    }
     
-    private Dotenv dotEnv;
-
-    public Config() {
-        Dotenv.configure().ignoreIfMissing();
-        this.dotEnv = Dotenv.load();        
-    }
-
-    /** 
-     * Returns an Ed25519PrivateKey from the OPERATOR_KEY environment variable
-     * @return Ed25519PrivateKey
-     */
-    public Ed25519PrivateKey getOperatorKey() {
-        return Ed25519PrivateKey.fromString(dotEnv.get("OPERATOR_KEY"));
-    }
-
-    /** 
-     * Returns a string representing the value of the OPERATOR_ID environment variable
-     * @return String
-     */
-    public String getOperatorAccount() {
-        return dotEnv.get("OPERATOR_ID");
-    }
-
-    /** 
-     * Returns a string representing the value of the OPERATOR_ID environment variable
-     * @return AccountId
-     */
-    public AccountId getOperatorAccountId() {
-        return AccountId.fromString(dotEnv.get("OPERATOR_ID"));
-    }
-
-    /** 
-     * Returns an AccountId representing the value of the OPERATOR_ID environment variable
-     * @return AccountId
-     */
-    public Map<AccountId, String> getNodesMap() {
-        
-        String nodes = dotEnv.get("NODES");
-        Map<AccountId, String> nodeList = new HashMap<AccountId, String>();
-        
-        String[] nodeData = nodes.split(",");
-        for (int i=0; i < nodeData.length; i++) {
-            nodeList.put(AccountId.fromString(nodeData[i+1]), nodeData[i]);
-            i++;
-        }
-        
-        return nodeList;
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
     }
 }

9d4a3748bb8e492eff641f4b86a1af808e681a87
Greg Scullard <gregscullard@hedera.com>
2019-10-25 11:02:25 +0200

    Update App.java

--- examples/simple-message-demo/src/main/java/simplemessagedemo/App.java
+++ examples/simple-message-demo/src/main/java/simplemessagedemo/App.java
@@ -25,15 +25,28 @@ public final class App
 //        });
 //        onHCSMessageCallback.triggerCallBack();
         
-        // HCSLib setup example
-        HCSLib hcsLib = new HCSLib()
-            .withEncryptedMessages(true)
-            .withKeyRotation(true, 10)
-            .withMessageSignature(true);
+        // Simplest setup and send
+        HCSLib hcsLib = new HCSLib();
         
         // Outbound message (app->lib->hedera example)
         try {
             Boolean success = new OutboundHCSMessage(hcsLib)
+                .sendMessage("test");
+        } catch (HederaNetworkException | IllegalArgumentException | HederaException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        
+        // More complex HCSLib setup example
+        HCSLib hcsLib2 = new HCSLib()
+            .withEncryptedMessages(true)
+            .withKeyRotation(true, 10)
+            .withMessageSignature(true);
+        
+        // More complex outbound message (app->lib->hedera example) with overrides
+        try {
+            Boolean success = new OutboundHCSMessage(hcsLib2)
                 .overrideEncryptedMessages(true)
                 .overrideKeyRotation(true, 10)
                 .overrideMessageSignature(false)

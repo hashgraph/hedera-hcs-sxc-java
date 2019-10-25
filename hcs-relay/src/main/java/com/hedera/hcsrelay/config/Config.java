36103828bedb017116c985c9311dcc5783d2b787
Greg Scullard <gregscullard@hedera.com>
2019-10-25 15:46:16 +0200

    Initial setup for relay to subscribe to mirror node
    -Config.yaml to list topics to listen to and mirror node address
    -TopicSubscriber class with constructor to subscribe to topics listed in config file

---
+++ hcs-relay/src/main/java/com/hedera/hcsrelay/config/Config.java
@@ -0,0 +1,35 @@
+package com.hedera.hcsrelay.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+/**
+ * Manages configuration
+ */
+
+public final class Config {
+    private YAMLConfig yamlConfig = new YAMLConfig();
+    
+    public Config() throws FileNotFoundException, IOException {
+        Yaml yaml = new Yaml(new Constructor(YAMLConfig.class));
+        
+        InputStream inputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("config.yaml");
+
+        File configFile = new File("./config.yaml");
+        if (configFile.exists()) {
+            // config file exists outside of jar, use it
+            inputStream = new FileInputStream(configFile.getCanonicalPath());
+        }
+        yamlConfig = yaml.load(inputStream);
+    }
+    public YAMLConfig getConfig() {
+        return this.yamlConfig;
+    }
+}
---
+++ hcs-relay/src/main/java/com/hedera/hcsrelay/subscribe/TopicsSubscriber.java
@@ -0,0 +1,24 @@
+package com.hedera.hcsrelay.subscribe;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.hedera.hashgraph.sdk.consensus.TopicId;
+import com.hedera.hcsrelay.config.Config;
+
+/**
+ * Subscribes to topic(s) against a mirror node
+ *
+ */
+public final class TopicsSubscriber {
+
+    public TopicsSubscriber() throws FileNotFoundException, IOException {
+        Config config = new Config();
+        
+        String mirrorAddress = config.getConfig().getMirrorAddress();
+        
+        for (TopicId topic : config.getConfig().getTopicIds()) {
+            // send subscription request to mirrorAddress for topic using the SDK
+            // likely needs a callback method
+        }
+    }
+}

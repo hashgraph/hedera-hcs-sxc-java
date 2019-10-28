618073a699ad13a21025957edd15c26c7dad7256
Nik S <nik@tapsw.com>
2019-10-28 09:24:42 +0000

    messaging setup

--- hcs-lib/src/main/java/com/hedera/hcslib/listeners/MQListener.java
+++ hcs-lib/src/main/java/com/hedera/hcslib/listeners/MQListener.java
@@ -5,22 +5,27 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+/*
+https://examples.javacodegeeks.com/enterprise-java/jms/jms-messagelistener-example/
+*/
+
 public class MQListener implements MessageListener{
-    private String consumerName;
- 
-    public MQListener(String consumerName) {
-        this.consumerName = consumerName;
+    private boolean acknowledge;
+    
+    public MQListener(boolean acknowledge) {
+        this.acknowledge = acknowledge;
     }
  
+    @Override
     public void onMessage(Message message) {
-        TextMessage textMessage = (TextMessage) message;
-        try {
-            System.out.println(consumerName + " received "
-                    + textMessage.getText());
-        } catch (JMSException e) {
-            e.printStackTrace();
+        if (acknowledge) {
+            try {
+                message.acknowledge();
+            } catch (JMSException e1) {
+                e1.printStackTrace();
+            }
         }
+        System.out.println(message);
     }
- 
 }
 

5d53eec19c7eddf4da5d811606b725f952167726
Nik S <nik@tapsw.com>
2019-11-04 11:37:59 +0000

    single par part messages

--- hcs-sxc-examples/simple-message-demo/src/main/java/com/hedera/hcsapp/HCSSend.java
+++ hcs-sxc-examples/simple-message-demo/src/main/java/com/hedera/hcsapp/HCSSend.java
@@ -9,6 +9,7 @@ import com.hedera.hcslib.HCSLib;
 import com.hedera.hcslib.callback.OnHCSMessageCallback;
 import com.hedera.hcslib.consensus.OutboundHCSMessage;
 import java.time.LocalDateTime;
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * Hello world!
@@ -21,14 +22,30 @@ public final class HCSSend {
       // Simplest setup and send
         HCSLib hcsLib = new HCSLib();
         
+        
         try {
-            Boolean success = 
+            
+            // example of a single part message been sent
+            
+            Boolean singlePartSuccess = 
                     new OutboundHCSMessage(hcsLib)
                    .overrideEncryptedMessages(false)
                    .overrideMessageSignature(false)
                    .sendMessage(0, "HCSSend - "+LocalDateTime.now().toString());
-            if (success) {
-                System.out.println("Message sent to HH network");
+            if (singlePartSuccess) {
+                System.out.println("Single part message sent to HH network");
+            }
+            
+            // example of a multi part message 
+            String random5kString = RandomStringUtils.random(5000, true, true);
+        
+            Boolean multiPartSuccess = 
+                    new OutboundHCSMessage(hcsLib)
+                   .overrideEncryptedMessages(false)
+                   .overrideMessageSignature(false)
+                   .sendMessage(0, "HCSSend multi part - "+random5kString);
+            if (multiPartSuccess) {
+                System.out.println("Multi part message sent to HH network");
             }
             
             // create a callback obect to receive the message

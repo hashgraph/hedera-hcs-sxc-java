c71a5b3c9e0c5d243bf9637102a19c1e2e7c557a
Nik S <nik@tapsw.com>
2019-11-07 12:36:15 +0000

    assembly disassembly tests complete

--- hcs-lib/src/test/java/com/hedera/hcslib/consensus/OutboundHCSMessageTest.java
+++ hcs-lib/src/test/java/com/hedera/hcslib/consensus/OutboundHCSMessageTest.java
@@ -1,5 +1,10 @@
 package com.hedera.hcslib.consensus;
 
+import com.hedera.hashgraph.sdk.TransactionId;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hcslib.proto.java.MessagePart;
+import java.util.List;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,16 +12,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OutboundHCSMessageTest {
     
+   
     public OutboundHCSMessageTest() {
     }
- 
-
+    
+    TransactionId txId ;
+    
+    @BeforeEach
+    public void steup(){
+        txId = new TransactionId(new AccountId(1234L));
+    }
+    
     @Test
-    public void testSomeMethod() {
+    public void testSingleChunking() {
+        List<MessagePart> chunks = OutboundHCSMessage.chunk(txId,"Single Chunk Message");
+        assertTrue(chunks.size() == 1);
         
-        //OutboundHCSMessage.chunk("hello");
-        
-        
+    }
+    
+    @Test
+    public void testMultiChunking() {
+        String longString = RandomStringUtils.random(5000, true, true);
+        List<MessagePart> chunks = OutboundHCSMessage.chunk(txId,longString);
+        assertTrue(chunks.size() == 2);
     }
     
 }

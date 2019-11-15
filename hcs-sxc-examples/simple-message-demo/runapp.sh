03536dd771fd3cef81ee44e4a2a91f85390aca18
Greg Scullard <gregscullard@hedera.com>
2019-11-15 12:59:29 +0100

    updates shell/command line scripts to 0.0.3

--- hcs-sxc-examples/simple-message-demo/runapp.sh
+++ hcs-sxc-examples/simple-message-demo/runapp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp ../../hcs-sxc-plugins/lib-persistence-in-memory/target/lib-persistence-in-memory-0.0.1-SNAPSHOT.jar ./target/lib
+cp ../../hcs-sxc-plugins/lib-persistence-in-memory/target/lib-persistence-in-memory-0.0.3-SNAPSHOT.jar ./target/lib
 cp .env ./target
 cd target
-java -cp 'simple-message-demo-0.0.1-SNAPSHOT.jar:lib/*' com.hedera.hcsapp.App $1
+java -cp 'simple-message-demo-0.0.3-SNAPSHOT.jar:lib/*' com.hedera.hcsapp.App $1
 

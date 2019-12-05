dbd7653177822f48436cf24fb55009ea11a4745c
N S <35506609+zoperado@users.noreply.github.com>
2019-12-05 15:18:58 +0000

    Ui scaffolding (#168)
    * Initial commit
    
    * First draft of demo Settlement-related messages
    
    Business-process messages to support simple Settlement demo
    
    * Adding missing TransactionID
    
    Copied/pasted AccountID and TransactionID.
    Replaced `charge` with `credit` to reduce ambuiguity.
    
    * Adding springboot
    
    * Springboot basics
    
    * init material ui
    
    * first draft
    
    * H2 database + sample data on http://localhost:8080/credits
    
    * collapsible item
    
    * collapsible datatables
    
    * inserts new credit
    
    * collapsible widget
    
    * bottom drawer implementation
    
    * prevent modal click bubble
    
    * new credit modal
    
    * fix button effects
    
    * dialog prefill for different recipients
    
    * form validation
    
    * validity select first fail
    
    * render panel from address book
    
    * templating setup
    
    * table templating
    
    * minor updates
    
    * minor
    
    * add await / async
    
    * minor updates
    
    * fix whoami
    
    * basic POST setup
    
    * post to network
    
    * minor fix
    
    * minor fix
    
    * snackbars
    
    * smooth send
    
    * bind confirm button to threadid
    
    * confirm credit
    
    * statemachine prpoposal
    
    * fix payer debtor receiver debtor naming
    
    * settlement ui
    
    * settlement calculation
    
    * create and post settlements
    
    * threadId truncation and camelcase status
    
    * error handling
    panel audit preps (not ready)
    
    * First Changes
    
    * Complete
    
    * syntax highlight
    
    * settlements

---
+++ hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/States.java
@@ -0,0 +1,42 @@
+package com.hedera.hcsapp;
+
+public enum States {
+    CREDIT_PROPOSED_PENDING("Credit Proposed (Pending)")
+    ,CREDIT_PROPOSED("Credit Proposed")
+    ,CREDIT_AGREED("Credit Agreed")
+    
+    ,SETTLEMENT_PROPOSED_PENDING("Settlement Proposed Pending")
+    ,SETTLEMENT_PROPOSED("Settlement Proposed")
+    ,SETTLEMENT_AGREED("Settlement Agreed")
+
+    ,SETTLE_INIT_PENDING("SETTLE_INIT_PENDING")
+    ,SETTLE_INIT_AWAIT_ACK("SETTLE_INIT_AWAIT_ACK")
+    ,SETTLE_INIT_ACK("SETTLE_INIT_ACK")
+
+    ,PAYMENT_INIT_PENDING("PAYMENT_INIT_PENDING")
+    ,PAYMENT_INIT_AWAIT_ACK("PAYMENT_INIT_AWAIT_ACK")
+    ,PAYMENT_INIT_ACK("PAYMENT_INIT_ACK")
+
+    ,PAYMENT_SENT_PENDING("PAYMENT_SENT_PENDING")
+    ,PAYMENT_SENT_AWAIT_ACK("PAYMENT_SENT_AWAIT_ACK")
+    ,PAYMENT_SENT_ACK("PAYMENT_SENT_ACK")
+
+    ,SETTLE_PAID_PENDING("SETTLE_PAID_PENDING")
+    ,SETTLE_PAID_AWAIT_ACK("SETTLE_PAID_AWAIT_ACK")
+    ,SETTLE_PAID_ACK("SETTLE_PAID_ACK")
+
+    ,SETTLE_COMPLETE_PENDING("SETTLE_COMPLETE_PENDING")
+    ,SETTLE_COMPLETE_AWAIT_ACK("SETTLE_COMPLETE_AWAIT_ACK")
+    ,SETTLE_COMPLETE_ACK("SETTLE_COMPLETE_ACK");
+    
+    private final String display;
+    
+    States(String display) {
+        this.display = display;
+    }
+    
+    public String getDisplay() {
+        return this.display;
+    }
+}
+
---
+++ hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/restclasses/SettlementRest.java
@@ -0,0 +1,60 @@
+package com.hedera.hcsapp.restclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hedera.hcsapp.AppData;
+import com.hedera.hcsapp.States;
+import com.hedera.hcsapp.entities.Settlement;
+import com.hedera.hcsapp.entities.SettlementItem;
+import com.hedera.hcsapp.repository.CreditRepository;
+import com.hedera.hcsapp.repository.SettlementItemRepository;
+
+import lombok.Data;
+
+@Data
+public final class SettlementRest {
+    
+    private String threadId;
+    private String applicationMessageId;
+    private String payerName;
+    private String recipientName;
+    private String additionalNotes;
+    private long netValue;
+    private String currency;
+    private String status;
+    private String createdDate;
+    private String createdTime;
+    private String topicId;
+    private String displayStatus;
+    
+    private List<CreditRest> credits = new ArrayList<CreditRest>();
+    private List<String> threadIds = new ArrayList<String>();
+    
+    public SettlementRest (Settlement settlement, AppData appData, SettlementItemRepository settlementItemRepository, CreditRepository creditRepository) {
+        this.threadId = settlement.getThreadId();
+        this.applicationMessageId = settlement.getApplicationMessageId();
+        this.payerName = settlement.getPayerName();
+        this.recipientName = settlement.getRecipientName();
+        this.additionalNotes = settlement.getAdditionalNotes();
+        this.netValue = settlement.getNetValue();
+        this.currency = settlement.getCurrency();
+        this.status = settlement.getStatus();
+        this.createdDate = settlement.getCreatedDate();
+        this.createdTime = settlement.getCreatedTime();
+        this.topicId = appData.getHCSLib().getTopicIds().get(appData.getTopicIndex()).toString();
+        this.displayStatus = States.valueOf(this.status).getDisplay().replace("Settlement ", ""); 
+
+        List<SettlementItem> settlementItemsFromDB = settlementItemRepository.findAllSettlementItems(settlement.getThreadId());
+        List<String> threadIds = new ArrayList<String>();
+        for (SettlementItem settlementItem : settlementItemsFromDB) {
+            this.threadIds.add(settlementItem.getId().getSettledThreadId());
+            creditRepository.findById(settlementItem.getId().getSettledThreadId()).ifPresent(
+                    (credit) -> {
+                        credits.add(new CreditRest(credit, appData));        
+                    }
+            );
+                
+        }
+    }
+}

33b4e840bfeb91eb7e2c263408faa59541efa20c
N S <35506609+zoperado@users.noreply.github.com>
2019-12-19 14:07:15 +0000

    Merge branch #254 fixing #253 (#260)
    * merge date time
    
    * fix state display issue
    
    * add payments menu
    
    * UI: Add support for Payment init processes #231
    
    * Revert "UI: Add support for Payment init processes #231"
    
    This reverts commit 843fa31f20257b5539e61ab95249d28879667aa3.
    
    * stash and stash pop database
    
    * minor fix
    
    * Fixes #246 Cross app database backup and restore
    Fixes #245  Channel selection bug
    
    * payment ack
    
    * minor fix
    
    * fuxes 234
    
    * added settlement display status to states
    
    * fix re-opened payment-sent-234
    
    * refactor init sent
    
    * fixes settlement payment made.
    fixes state names
    
    * settlementCompletedDialog done
    
    * ack done

--- hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/States.java
+++ hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/States.java
@@ -1,49 +1,54 @@
 package com.hedera.hcsapp;
 
 public enum States {
-    CREDIT_PROPOSED_PENDING("Credit Proposed (Pending)")
-    ,CREDIT_PROPOSED("Credit Proposed")
-    ,CREDIT_AGREED_PENDING("Credit Agreed (Pending)")
-    ,CREDIT_AGREED("Credit Agreed")
+    CREDIT_PROPOSED_PENDING("Proposed (Pending)", "N/A")
+    ,CREDIT_PROPOSED("Proposed", "N/A")
+    ,CREDIT_AGREED_PENDING("Agreed (Pending)", "N/A")
+    ,CREDIT_AGREED("Agreed", "N/A")
     
-    ,SETTLEMENT_PROPOSED_PENDING("Settlement Proposed (Pending)")
-    ,SETTLEMENT_PROPOSED("Settlement Proposed")
-    ,SETTLEMENT_AGREED_PENDING("Settlement Agreed (Pending)")
-    ,SETTLEMENT_AGREED("Settlement Agreed")
+    ,SETTLEMENT_PROPOSED_PENDING("Settlement Proposed (Pending)", "Proposed (Pending)")
+    ,SETTLEMENT_PROPOSED("Settlement Proposed", "Proposed")
+    ,SETTLEMENT_AGREED_PENDING("Settling", "Agreed (Pending)")
+    ,SETTLEMENT_AGREED("Settling", "Agreed")
 
-    ,SETTLE_INIT_AWAIT_ACK_PENDING("Settlement Payment Channel Proposed (Pending)")
-    ,SETTLE_INIT_AWAIT_ACK("Settlement Payment Channel Proposed")
-    ,SETTLE_INIT_ACK_PENDING("Settlement Payment Channel Agreed (Pending)")
-    ,SETTLE_INIT_ACK("Settlement Payment Channel Agreed")
+    ,SETTLE_INIT_AWAIT_ACK_PENDING("Settling", "Payment Channel Proposed (Pending)")
+    ,SETTLE_INIT_AWAIT_ACK("Settling", "Payment Channel Proposed")
+    ,SETTLE_INIT_ACK_PENDING("Settling", "Payment Channel Agreed (Pending)")
+    ,SETTLE_INIT_ACK("Settling", "Payment Channel Agreed")
 
-    ,PAYMENT_INIT_AWAIT_ACK_PENDING("Payment Proposed (Pending)")
-    ,PAYMENT_INIT_AWAIT_ACK("Payment Proposed")
-    ,PAYMENT_INIT_ACK_PENDING("Payment Agreed (Pending)")
-    ,PAYMENT_INIT_ACK("Payment Agreed")
+    ,PAYMENT_INIT_AWAIT_ACK_PENDING("Settling", "Payment Proposed (Pending)")
+    ,PAYMENT_INIT_AWAIT_ACK("Settling", "Payment Proposed")
+    ,PAYMENT_INIT_ACK_PENDING("Settling", "Payment Agreed (Pending)")
+    ,PAYMENT_INIT_ACK("Settling", "Payment Agreed")
 
-    ,PAYMENT_SENT_AWAIT_ACK_PENDING("Payment Sent (Pending)")
-    ,PAYMENT_SENT_AWAIT_ACK("Payment Sent")
-    ,PAYMENT_SENT_ACK_PENDING("Payment Sent Agreed (Pending)")
-    ,PAYMENT_SENT_ACK("Payment Sent Agreed")
-    //==
-    ,SETTLE_PAID_AWAIT_ACK_PENDING("Settlement Payment Made (Pending)")
-    ,SETTLE_PAID_AWAIT_ACK("Settlement Payment Made")
-    ,SETTLE_PAID_ACK_PENDING("Settlement Payment Agreed (Pending)")
-    ,SETTLE_PAID_ACK("Settlement Payment Agreed")
+    ,PAYMENT_SENT_AWAIT_ACK_PENDING("Settling", "Payment Made (Pending)")
+    ,PAYMENT_SENT_AWAIT_ACK("Settling", "Payment Made")
+    ,PAYMENT_SENT_ACK_PENDING("Settling", "Payment Agreed (Pending)")
+    ,PAYMENT_SENT_ACK("Settling", "Payment Agreed")
 
-    ,SETTLE_COMP_AWAIT_ACK_PENDING("Settlement Completed (Pending)")
-    ,SETTLE_COMP_AWAIT_ACK("Settlement Completed")
-    ,SETTLE_COMPLETE_ACK_PENDING("Settlement Completed Agreed (Pending)")
-    ,SETTLE_COMPLETE_ACK("Settlement Completed Agreed");
+    ,SETTLE_PAID_AWAIT_ACK_PENDING("Settling", "Payment Acknowledged (Pending)")
+    ,SETTLE_PAID_AWAIT_ACK("Settling", "Payment Acknowledged")
+    ,SETTLE_PAID_ACK_PENDING("Settling", "Receipt Requested (Pending)")
+    ,SETTLE_PAID_ACK("Settling", "Receipt Requested")
+
+    ,SETTLE_COMP_AWAIT_ACK_PENDING("Settling", "Receipt Confirmed (Pending)")
+    ,SETTLE_COMP_AWAIT_ACK("Settling", "Receipt Confirmed")
+    ,SETTLE_COMPLETE_ACK_PENDING("Settling", "Complete (Pending)")
+    ,SETTLE_COMPLETE_ACK("Settled", "Complete");
     
-    private final String display;
+    private final String displayForCredit;
+    private final String displayForSettlement;
     
-    States(String display) {
-        this.display = display;
+    States(String displayForCredit, String displayForSettlement) {
+        this.displayForCredit = displayForCredit;
+        this.displayForSettlement = displayForSettlement;
     }
     
-    public String getDisplay() {
-        return this.display;
+    public String getDisplayForCredit() {
+        return this.displayForCredit;
+    }
+    public String getDisplayForSettlement() {
+        return this.displayForSettlement;
     }
 }
 

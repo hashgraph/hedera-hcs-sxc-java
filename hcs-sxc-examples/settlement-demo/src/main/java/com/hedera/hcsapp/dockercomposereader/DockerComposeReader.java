0a54018876141be0bf0e40a248cdd1bfd3ff7b84
gregscullard <greg@scullard.com>
2020-01-07 13:26:38 +0100

    Lighten docker images (#268)
    * simple landing page + consolidation of apps.yaml into docker-compose.yml
    
    * Makes docker images lighter by only copying necessary files.
    
    * moves all docker-compose files to src/main/resources
    
    * updates to cater for files in /src/main/resources

--- hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/dockercomposereader/DockerComposeReader.java
+++ hcs-sxc-examples/settlement-demo/src/main/java/com/hedera/hcsapp/dockercomposereader/DockerComposeReader.java
@@ -7,23 +7,37 @@ import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.representer.Representer;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Manages configuration
  */
 
+@Log4j2
 public final class DockerComposeReader {
     
     public static DockerCompose parse() throws Exception {
-        InputStream inputStream;
+        InputStream inputStream = null;
+        
+        log.info("Loading app net configuration from ./docker-compose.yml");
         File configFile = new File("./docker-compose.yml");
         if (configFile.exists()) {
+            log.info("Found app net configuration in ./docker-compose.yml");
             inputStream = new FileInputStream(configFile.getCanonicalPath());
+        } else {
+            log.info("Loading app net configuration from ./src/main/resource/docker-compose.yml");
+            inputStream = DockerComposeReader.class
+                .getClassLoader()
+                .getResourceAsStream("docker-compose.yml");
+        
+        }
+        if (inputStream != null) {
             Representer representer = new Representer();
             representer.getPropertyUtils().setSkipMissingProperties(true);
             Yaml yaml = new Yaml(new Constructor(DockerCompose.class),representer);
             return yaml.load(inputStream);
         } else {
-            throw new Exception("Docker compose file not found.");
+            throw new Exception("Docker compose file not found in ./ or ./src/main/resources.");
         }
     }
 }

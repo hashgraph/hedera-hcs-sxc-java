55e8359cebb17561290d783ab50f84a013166772
gregscullard <greg@scullard.com>
2020-01-07 14:55:33 +0100

    270-Documentation (#271)
    * Initial documentation in readme.md
    
    * Complete
    
    Signed-off-by: zoperado <zoperado@tapsw.com>

--- hcs-sxc-examples/simple-message-demo/runapp.sh
+++ hcs-sxc-examples/simple-message-demo/runapp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp ../../hcs-sxc-plugins/lib-persistence-in-memory/target/lib-persistence-in-memory-0.0.3-SNAPSHOT.jar ./target/lib
+cp ../../hcs-sxc-plugins/lib-mirror-queue-artemis/target/lib-mirror-queue-artemis-0.0.3-SNAPSHOT.jar ./target/lib
 cp .env ./target
 cd target
-java -cp 'simple-message-demo-0.0.3-SNAPSHOT.jar:lib/*' com.hedera.hcsapp.App $1
+java -Dcom.hedera.hashgraph.sdk.experimental=true -cp 'simple-message-demo-0.0.3-SNAPSHOT.jar:lib/*' com.hedera.hcsapp.App $1
 

1e0e85e23d451d331a008c3ea1eba8f123b6601c
Nik S <nik@tapsw.com>
2020-01-13 22:42:51 +0000

    environment variables in command line override .env file

--- hcs-sxc-examples/hcs-sxc-settlement-demo/src/main/java/com/hedera/hcsapp/AppData.java
+++ hcs-sxc-examples/hcs-sxc-settlement-demo/src/main/java/com/hedera/hcsapp/AppData.java
@@ -15,6 +15,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public final class AppData {
+
     private static HCSCore hcsCore;
     private int topicIndex = 0; // refers to the first topic ID in the config.yaml
     private String privateKey = "";
@@ -24,31 +25,34 @@ public final class AppData {
     List<AppClient> appClients = new ArrayList<>();
 
     public AppData() throws Exception {
-        Dotenv dotEnv = Dotenv.configure().ignoreIfMissing().load();
-
-        if ((dotEnv.get("APP_ID") == null) || (dotEnv.get("APP_ID").isEmpty())) {
-            // no environment variables found in environment or ./.env, try ./src/main/resource/.env
-            dotEnv = Dotenv.configure().directory("./src/main/resources").ignoreIfMissing().load();
+        if (System.getProperty("APP_ID")!=null && System.getProperty("OPERATOR_KEY")!=null) {
+            this.appId = Long.parseLong(System.getProperty("APP_ID"));
+            this.privateKey = System.getProperty("OPERATOR_KEY");
+        } else {
+            Dotenv dotEnv = Dotenv.configure().ignoreIfMissing().load();
+            if ((dotEnv.get("APP_ID") == null) || (dotEnv.get("APP_ID").isEmpty())) {
+                // no environment variables found in environment or ./.env, try ./src/main/resource/.env
+                dotEnv = Dotenv.configure().directory("./src/main/resources").ignoreIfMissing().load();
+            }
+            if ((dotEnv.get("APP_ID") == null) || (dotEnv.get("APP_ID").isEmpty())) {
+                log.error("APPID environment variable is not set");
+                log.error("APPID environment variable not found in ./.env");
+                log.error("APPID environment variable not found in ./src/main/resources");
+                throw new Exception("APPID environment variable is not set - exiting");
+            }
+            if ((dotEnv.get("OPERATOR_KEY") == null) || (dotEnv.get("OPERATOR_KEY").isEmpty())) {
+                log.error("OPERATOR_KEY environment variable is not set");
+                log.error("OPERATOR_KEY environment variable not found in ./.env");
+                log.error("OPERATOR_KEY environment variable not found in ./src/main/resources");
+                throw new Exception("OPERATOR_KEY environment variable is not set - exiting");
+            }
+            this.appId = Long.parseLong(dotEnv.get("APP_ID"));
+            this.privateKey = dotEnv.get("OPERATOR_KEY");
         }
-
-        if ((dotEnv.get("APP_ID") == null) || (dotEnv.get("APP_ID").isEmpty())) {
-            log.error("APPID environment variable is not set");
-            log.error("APPID environment variable not found in ./.env");
-            log.error("APPID environment variable not found in ./src/main/resources");
-            throw new Exception("APPID environment variable is not set - exiting");
-        }
-        if ((dotEnv.get("OPERATOR_KEY") == null) || (dotEnv.get("OPERATOR_KEY").isEmpty())) {
-            log.error("OPERATOR_KEY environment variable is not set");
-            log.error("OPERATOR_KEY environment variable not found in ./.env");
-            log.error("OPERATOR_KEY environment variable not found in ./src/main/resources");
-            throw new Exception("OPERATOR_KEY environment variable is not set - exiting");
-        }
-
+        
+        AppData.hcsCore = new HCSCore(appId);
         DockerCompose dockerCompose = DockerComposeReader.parse();
 
-        this.appId = Long.parseLong(dotEnv.get("APP_ID"));
-        AppData.hcsCore = new HCSCore(appId);
-        this.privateKey = dotEnv.get("OPERATOR_KEY");
         this.publicKey = dockerCompose.getPublicKeyForId(this.appId);
         this.userName = dockerCompose.getNameForId(this.appId);
         this.topicIndex = 0;
@@ -75,21 +79,27 @@ public final class AppData {
     public HCSCore getHCSCore() {
         return AppData.hcsCore;
     }
+
     public long getAppId() {
         return this.appId;
     }
+
     public String getPrivateKey() {
         return this.privateKey;
     }
+
     public String getPublicKey() {
         return this.publicKey;
     }
+
     public String getUserName() {
         return this.userName;
     }
+
     public int getTopicIndex() {
         return this.topicIndex;
     }
+
     public List<AppClient> getAppClients() {
         return this.appClients;
     }

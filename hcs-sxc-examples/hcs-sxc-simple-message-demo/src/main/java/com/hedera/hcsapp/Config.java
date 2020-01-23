c68a8573a2e112f49406a4ca717e98401a3d634f
Greg Scullard <gregscullard@hedera.com>
2020-01-23 10:05:33 +0100

    config files in ./config

--- hcs-sxc-examples/hcs-sxc-simple-message-demo/src/main/java/com/hedera/hcsapp/Config.java
+++ hcs-sxc-examples/hcs-sxc-simple-message-demo/src/main/java/com/hedera/hcsapp/Config.java
@@ -8,10 +8,12 @@ import java.io.InputStream;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Manages configuration
  */
-
+@Log4j2
 public final class Config {
     private AppYAML yamlConfig = new AppYAML();
     
@@ -22,11 +24,23 @@ public final class Config {
                 .getClassLoader()
                 .getResourceAsStream("apps.yaml");
         
-        File configFile = new File("./apps.yaml");
+        File configFile = new File("./config/apps.yaml");
         if (configFile.exists()) {
+            log.info("Loading apps.yaml from ./config");
             // config file exists outside of jar, use it
             inputStream = new FileInputStream(configFile.getCanonicalPath());
-        } 
+        } else {
+
+            configFile = new File("./apps.yaml");
+            if (configFile.exists()) {
+                log.info("Loading apps.yaml from ./");
+                // config file exists outside of jar, use it
+                inputStream = new FileInputStream(configFile.getCanonicalPath());
+            } else {
+                log.info("Loading apps.yaml from src/main/resources");
+            }
+        }
+
         yamlConfig = yaml.load(inputStream);
     }
     public AppYAML getConfig() {

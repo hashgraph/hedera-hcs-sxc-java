a30d18a9300bccc62a4ebe5cdb13903a2f3e4226
Greg Scullard <gregscullard@hedera.com>
2020-01-23 10:05:33 +0100

    config files in ./config
    Signed-off-by: zoperado <zoperado@tapsw.com>

--- hcs-sxc-core/src/main/java/com/hedera/hcs/sxc/config/Config.java
+++ hcs-sxc-core/src/main/java/com/hedera/hcs/sxc/config/Config.java
@@ -5,12 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;

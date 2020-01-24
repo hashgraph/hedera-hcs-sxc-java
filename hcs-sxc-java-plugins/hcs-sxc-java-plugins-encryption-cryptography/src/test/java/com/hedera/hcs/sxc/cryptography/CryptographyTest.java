97a4ba02f04fb1373cea4ada9a28fbede4101848
Nik S <nik@tapsw.com>
2020-01-24 19:25:40 +0000

    FAIL2 factored out Cryptography module  Merge checkpoint Merge branch 'plugins-encryption-key-rotation' of https://github.com/hashgraph/hcs-lib into local-master
    # Conflicts:
    #	hcs-sxc-java-core/src/main/java/com/hedera/hcs/sxc/HCSCore.java
    #	hcs-sxc-java-core/src/main/java/com/hedera/hcs/sxc/cryptography/Cryptography.java
    #	hcs-sxc-java-core/src/main/java/com/hedera/hcs/sxc/cryptography/KeyRotation.java
    #	hcs-sxc-java-core/src/test/java/com/hedera/hcs/sxc/cryptography/CryptographyTest.java
    #	hcs-sxc-java-core/src/test/java/com/hedera/hcs/sxc/cryptography/KeyRotationTest.java
    #	hcs-sxc-java-examples/hcs-sxc-java-settlement-demo/pom.xml
    #	hcs-sxc-java-examples/hcs-sxc-java-simple-message-demo/pom.xml
    #	hcs-sxc-java-plugins/hcs-sxc-java-plugins-mirror-direct/pom.xml
    #	hcs-sxc-java-plugins/pom.xml
    Signed-off-by: zoperado <zoperado@tapsw.com>

---
+++ hcs-sxc-java-plugins/hcs-sxc-java-plugins-encryption-cryptography/src/test/java/com/hedera/hcs/sxc/cryptography/CryptographyTest.java
@@ -0,0 +1,51 @@
+package com.hedera.hcs.sxc.cryptography;
+
+import com.hedera.hcs.sxc.utils.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.security.KeyPair;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CryptographyTest {
+    
+    static byte[] secretKey;
+    String cleartext = "Hear my cries Hear 234sdf! �$%&*)_+ my call Lend me your ears See my fall See my error Know my faults Time halts See my loss ";
+    KeyPair generateRsaKeyPair = null;
+    byte[] privateKeyBytes = null;
+    String privateKeyHexEncoded = null;
+    byte[] publicKeyBytes = null;
+    String publicKeyKexEncoded = null;
+    
+    public CryptographyTest() {
+    }
+
+    @BeforeAll
+    public static void initClass(){
+        try {
+            KeyPair kp = Cryptography.generateRsaKeyPair();
+            secretKey =  kp.getPrivate().getEncoded();
+        } catch (Exception ex) {
+            Logger.getLogger(CryptographyTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    @Test
+    public void encryptAndDecrypt() throws Exception {
+        byte[] encrypt = Cryptography.encrypt(secretKey, cleartext);
+        String encryptHex = StringUtils.byteArrayToHexString(encrypt);
+        byte[] encryptPrime = StringUtils.hexStringToByteArray(encryptHex);
+        assertArrayEquals(encrypt, encryptPrime);
+        byte[] decrypt = Cryptography.decrypt(secretKey, encrypt);
+        assertTrue(Arrays.equals(StringUtils.stringToByteArray(cleartext), decrypt)); 
+        assertEquals(cleartext, StringUtils.byteArrayToString(decrypt));
+        assertEquals(cleartext, Cryptography.decryptToClearText(secretKey, encrypt));
+    }
+}

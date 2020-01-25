42a29186547908d187f9ff2d544095462dc14bbe
Nik S <nik@tapsw.com>
2020-01-25 16:17:29 +0000

    fix config parrse errors

---
+++ hcs-sxc-java-plugins/hcs-sxc-java-plugins-cryptography/src/test/java/com/hedera/hcs/sxc/plugin/cryptography/keyrotation/KeyRotationTest.java
@@ -0,0 +1,24 @@
+package com.hedera.hcs.sxc.plugin.cryptography.keyrotation;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+
+
+public class KeyRotationTest {
+    
+    public KeyRotationTest() {
+    }
+
+    @Test
+    public void createCommonSecret() {
+        KeyRotation keyRotation = new KeyRotation();
+        byte[] alicePublic = keyRotation.aliceFirst();
+        Pair<byte[], byte[]> bobPubSecret = KeyRotation.bobGenFromAlice(alicePublic);
+        byte[] aliceSharedSecret = keyRotation.aliceFinish(bobPubSecret.getLeft());
+        byte[] bobSharedSecret = bobPubSecret.getRight();
+        assertTrue(Arrays.equals(aliceSharedSecret, bobSharedSecret));
+    }
+}

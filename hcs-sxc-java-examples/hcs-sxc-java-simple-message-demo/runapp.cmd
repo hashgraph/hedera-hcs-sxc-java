ff78fada46b80e365eadd2dbaa8cc3765a33d775
gregscullard <greg@scullard.com>
2020-02-03 15:18:34 +0100

    Simplifies simple-demo and tidies up some of the pom versions (#369)
    * Simplifies simple-demo and tidies up some of the pom versions
    
    * Update CHANGELOG.md
    
    * Update config.yaml.sample
    
    Signed-off-by: zoperado <zoperado@tapsw.com>

--- hcs-sxc-java-examples/hcs-sxc-java-simple-message-demo/runapp.cmd
+++ hcs-sxc-java-examples/hcs-sxc-java-simple-message-demo/runapp.cmd
@@ -1,5 +1,1 @@
-copy ..\..\hcs-sxc-java-plugins\hcs-sxc-java-plugins-persistence-in-memory\target\hcs-sxc-java-plugins-persistence-in-memory-0.0.3-SNAPSHOT.jar target\lib
-copy ..\..\hcs-sxc-java-plugins\hcs-sxc-java-plugins-mirror-queue-artemis\target\hcs-sxc-java-plugins-mirror-queue-artemis-0.0.3-SNAPSHOT.jar target\lib
-xcopy /i /h /y config\*.* target\config
-cd target
-java -cp 'hcs-sxc-java-simple-message-demo-0.0.3-SNAPSHOT.jar:lib/*' com.hedera.hcsapp.App $1
+java -jar hcs-sxc-java-simple-message-demo-0.0.3-SNAPSHOT-shaded.jar $1

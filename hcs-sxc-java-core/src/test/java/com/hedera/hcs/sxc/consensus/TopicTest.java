45f51f50c3733ca80fe40d9fc21b70492f164056
Greg Scullard <gregscullard@hedera.com>
2020-02-18 16:43:14 +0100

    Remove "Override", replace with "with"
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hcs-sxc-java-core/src/test/java/com/hedera/hcs/sxc/consensus/TopicTest.java
+++ hcs-sxc-java-core/src/test/java/com/hedera/hcs/sxc/consensus/TopicTest.java
@@ -40,23 +40,25 @@ public class TopicTest {
         Ed25519PrivateKey adminKey = Ed25519PrivateKey.generate();
         Ed25519PrivateKey submitKey = Ed25519PrivateKey.generate();
         HCSCore hcsCore = HCSCore.INSTANCE
-                .singletonInstanceWithAppIdEnvAndConfig(0, "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test");
+                .singletonInstance("0", "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test")
+                .withOperatorAccountId(AccountId.fromString("0.0.2"))
+                .withOperatorKey(ed25519PrivateKey.fromString("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65"));
 
         CreateHCSTopic createHCSTopic = new CreateHCSTopic(hcsCore);
 
-        assertEquals(4, createHCSTopic.getOverrideNodeMap().size());
-        assertEquals("0.testnet.hedera.com:50211", createHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.3")));
-        assertEquals("1.testnet.hedera.com:50211", createHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.4")));
-        assertEquals("2.testnet.hedera.com:50211", createHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.5")));
-        assertEquals("3.testnet.hedera.com:50211", createHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.6")));
-        assertEquals("0.0.2", createHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", createHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(4, createHCSTopic.getNodeMap().size());
+        assertEquals("0.testnet.hedera.com:50211", createHCSTopic.getNodeMap().get(AccountId.fromString("0.0.3")));
+        assertEquals("1.testnet.hedera.com:50211", createHCSTopic.getNodeMap().get(AccountId.fromString("0.0.4")));
+        assertEquals("2.testnet.hedera.com:50211", createHCSTopic.getNodeMap().get(AccountId.fromString("0.0.5")));
+        assertEquals("3.testnet.hedera.com:50211", createHCSTopic.getNodeMap().get(AccountId.fromString("0.0.6")));
+        assertEquals("0.0.2", createHCSTopic.getOperatorAccountId().toString());
+        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", createHCSTopic.getOperatorKey().toString());
 
         // override defaults
         createHCSTopic
-            .overrideNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
-            .overrideOperatorAccountId(AccountId.fromString("0.0.10"))
-            .overrideOperatorKey(ed25519PrivateKey)
+            .withNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
+            .withOperatorAccountId(AccountId.fromString("0.0.10"))
+            .withOperatorKey(ed25519PrivateKey)
             .setAdminKey(adminKey.publicKey)
             .setAutoRenewAccountId(AccountId.fromString("0.0.11"))
             .setAutoRenewAccountId("0.0.55")
@@ -68,10 +70,10 @@ public class TopicTest {
         
         // test updated values
         // note some values are allocated to a Transaction object inside createHCSTopic which don't have getters
-        assertEquals(1, createHCSTopic.getOverrideNodeMap().size());
-        assertEquals("testing.hedera.com", createHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.19")));
-        assertEquals("0.0.10", createHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals(ed25519PrivateKey.toString(), createHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(1, createHCSTopic.getNodeMap().size());
+        assertEquals("testing.hedera.com", createHCSTopic.getNodeMap().get(AccountId.fromString("0.0.19")));
+        assertEquals("0.0.10", createHCSTopic.getOperatorAccountId().toString());
+        assertEquals(ed25519PrivateKey.toString(), createHCSTopic.getOperatorKey().toString());
     }
 
     @Test
@@ -80,23 +82,25 @@ public class TopicTest {
         Ed25519PrivateKey adminKey = Ed25519PrivateKey.generate();
         Ed25519PrivateKey submitKey = Ed25519PrivateKey.generate();
          HCSCore hcsCore = HCSCore.INSTANCE
-                .singletonInstanceWithAppIdEnvAndConfig(0, "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test");
+                .singletonInstance("0", "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test")
+                .withOperatorAccountId(AccountId.fromString("0.0.2"))
+                .withOperatorKey(ed25519PrivateKey.fromString("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65"));
 
         UpdateHCSTopic updateHCSTopic = new UpdateHCSTopic(hcsCore);
 
-        assertEquals(4, updateHCSTopic.getOverrideNodeMap().size());
-        assertEquals("0.testnet.hedera.com:50211", updateHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.3")));
-        assertEquals("1.testnet.hedera.com:50211", updateHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.4")));
-        assertEquals("2.testnet.hedera.com:50211", updateHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.5")));
-        assertEquals("3.testnet.hedera.com:50211", updateHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.6")));
-        assertEquals("0.0.2", updateHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", updateHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(4, updateHCSTopic.getNodeMap().size());
+        assertEquals("0.testnet.hedera.com:50211", updateHCSTopic.getNodeMap().get(AccountId.fromString("0.0.3")));
+        assertEquals("1.testnet.hedera.com:50211", updateHCSTopic.getNodeMap().get(AccountId.fromString("0.0.4")));
+        assertEquals("2.testnet.hedera.com:50211", updateHCSTopic.getNodeMap().get(AccountId.fromString("0.0.5")));
+        assertEquals("3.testnet.hedera.com:50211", updateHCSTopic.getNodeMap().get(AccountId.fromString("0.0.6")));
+        assertEquals("0.0.2", updateHCSTopic.getOperatorAccountId().toString());
+        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", updateHCSTopic.getOperatorKey().toString());
 
         // override defaults
         updateHCSTopic
-            .overrideNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
-            .overrideOperatorAccountId(AccountId.fromString("0.0.10"))
-            .overrideOperatorKey(ed25519PrivateKey)
+            .withNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
+            .withOperatorAccountId(AccountId.fromString("0.0.10"))
+            .withOperatorKey(ed25519PrivateKey)
             .setAdminKey(adminKey.publicKey)
             .setAutoRenewAccountId(AccountId.fromString("0.0.11"))
             .setAutoRenewPeriod(Duration.ofMillis(1000))
@@ -116,33 +120,35 @@ public class TopicTest {
         
         // test updated values
         // note some values are allocated to a Transaction object inside createHCSTopic which don't have getters
-        assertEquals(1, updateHCSTopic.getOverrideNodeMap().size());
-        assertEquals("testing.hedera.com", updateHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.19")));
-        assertEquals("0.0.10", updateHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals(ed25519PrivateKey.toString(), updateHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(1, updateHCSTopic.getNodeMap().size());
+        assertEquals("testing.hedera.com", updateHCSTopic.getNodeMap().get(AccountId.fromString("0.0.19")));
+        assertEquals("0.0.10", updateHCSTopic.getOperatorAccountId().toString());
+        assertEquals(ed25519PrivateKey.toString(), updateHCSTopic.getOperatorKey().toString());
     }
 
     @Test
     public void testDeleteTopic() throws Exception {
         Ed25519PrivateKey ed25519PrivateKey = Ed25519PrivateKey.generate();
          HCSCore hcsCore = HCSCore.INSTANCE
-                .singletonInstanceWithAppIdEnvAndConfig(0, "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test");
+                .singletonInstance("0", "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test")
+                .withOperatorAccountId(AccountId.fromString("0.0.2"))
+                .withOperatorKey(ed25519PrivateKey.fromString("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65"));
 
         DeleteHCSTopic deleteHCSTopic = new DeleteHCSTopic(hcsCore);
 
-        assertEquals(4, deleteHCSTopic.getOverrideNodeMap().size());
-        assertEquals("0.testnet.hedera.com:50211", deleteHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.3")));
-        assertEquals("1.testnet.hedera.com:50211", deleteHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.4")));
-        assertEquals("2.testnet.hedera.com:50211", deleteHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.5")));
-        assertEquals("3.testnet.hedera.com:50211", deleteHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.6")));
-        assertEquals("0.0.2", deleteHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", deleteHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(4, deleteHCSTopic.getNodeMap().size());
+        assertEquals("0.testnet.hedera.com:50211", deleteHCSTopic.getNodeMap().get(AccountId.fromString("0.0.3")));
+        assertEquals("1.testnet.hedera.com:50211", deleteHCSTopic.getNodeMap().get(AccountId.fromString("0.0.4")));
+        assertEquals("2.testnet.hedera.com:50211", deleteHCSTopic.getNodeMap().get(AccountId.fromString("0.0.5")));
+        assertEquals("3.testnet.hedera.com:50211", deleteHCSTopic.getNodeMap().get(AccountId.fromString("0.0.6")));
+        assertEquals("0.0.2", deleteHCSTopic.getOperatorAccountId().toString());
+        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", deleteHCSTopic.getOperatorKey().toString());
 
         // override defaults
         deleteHCSTopic
-            .overrideNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
-            .overrideOperatorAccountId(AccountId.fromString("0.0.10"))
-            .overrideOperatorKey(ed25519PrivateKey)
+            .withNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
+            .withOperatorAccountId(AccountId.fromString("0.0.10"))
+            .withOperatorKey(ed25519PrivateKey)
             .setMaxTransactionFee(100L)
             .setTopicId(ConsensusTopicId.fromString("0.0.44"))
             .setTopicId("0.0.55")
@@ -150,42 +156,44 @@ public class TopicTest {
         
         // test updated values
         // note some values are allocated to a Transaction object inside createHCSTopic which don't have getters
-        assertEquals(1, deleteHCSTopic.getOverrideNodeMap().size());
-        assertEquals("testing.hedera.com", deleteHCSTopic.getOverrideNodeMap().get(AccountId.fromString("0.0.19")));
-        assertEquals("0.0.10", deleteHCSTopic.getOverrideOperatorAccountId().toString());
-        assertEquals(ed25519PrivateKey.toString(), deleteHCSTopic.getOverrideOperatorKey().toString());
+        assertEquals(1, deleteHCSTopic.getNodeMap().size());
+        assertEquals("testing.hedera.com", deleteHCSTopic.getNodeMap().get(AccountId.fromString("0.0.19")));
+        assertEquals("0.0.10", deleteHCSTopic.getOperatorAccountId().toString());
+        assertEquals(ed25519PrivateKey.toString(), deleteHCSTopic.getOperatorKey().toString());
     }
 
     @Test
     public void testGetInfoTopic() throws Exception {
         Ed25519PrivateKey ed25519PrivateKey = Ed25519PrivateKey.generate();
          HCSCore hcsCore = HCSCore.INSTANCE
-                .singletonInstanceWithAppIdEnvAndConfig(0, "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test");
+                .singletonInstance("0", "./src/test/resources/config.yaml", "./src/test/resources/dotenv.test")
+                .withOperatorAccountId(AccountId.fromString("0.0.2"))
+                .withOperatorKey(ed25519PrivateKey.fromString("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65"));
 
         GetTopicInfo getTopicInfo = new GetTopicInfo(hcsCore);
 
-        assertEquals(4, getTopicInfo.getOverrideNodeMap().size());
-        assertEquals("0.testnet.hedera.com:50211", getTopicInfo.getOverrideNodeMap().get(AccountId.fromString("0.0.3")));
-        assertEquals("1.testnet.hedera.com:50211", getTopicInfo.getOverrideNodeMap().get(AccountId.fromString("0.0.4")));
-        assertEquals("2.testnet.hedera.com:50211", getTopicInfo.getOverrideNodeMap().get(AccountId.fromString("0.0.5")));
-        assertEquals("3.testnet.hedera.com:50211", getTopicInfo.getOverrideNodeMap().get(AccountId.fromString("0.0.6")));
-        assertEquals("0.0.2", getTopicInfo.getOverrideOperatorAccountId().toString());
-        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", getTopicInfo.getOverrideOperatorKey().toString());
+        assertEquals(4, getTopicInfo.getNodeMap().size());
+        assertEquals("0.testnet.hedera.com:50211", getTopicInfo.getNodeMap().get(AccountId.fromString("0.0.3")));
+        assertEquals("1.testnet.hedera.com:50211", getTopicInfo.getNodeMap().get(AccountId.fromString("0.0.4")));
+        assertEquals("2.testnet.hedera.com:50211", getTopicInfo.getNodeMap().get(AccountId.fromString("0.0.5")));
+        assertEquals("3.testnet.hedera.com:50211", getTopicInfo.getNodeMap().get(AccountId.fromString("0.0.6")));
+        assertEquals("0.0.2", getTopicInfo.getOperatorAccountId().toString());
+        assertEquals("302e020100300506032b657004220420abb9499694bad1f081cb2a55a08989303cbc3322fae657db1044fdbf3b9eed65", getTopicInfo.getOperatorKey().toString());
 
         // override defaults
         getTopicInfo
-            .overrideNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
-            .overrideOperatorAccountId(AccountId.fromString("0.0.10"))
-            .overrideOperatorKey(ed25519PrivateKey)
+            .withNodeMap(Map.of(AccountId.fromString("0.0.19"),"testing.hedera.com"))
+            .withOperatorAccountId(AccountId.fromString("0.0.10"))
+            .withOperatorKey(ed25519PrivateKey)
             .setTopicId(ConsensusTopicId.fromString("0.0.44"))
             .setTopicId("0.0.55")
             .setMaxQueryPayment(1000L);
         
         // test updated values
         // note some values are allocated to a Transaction object inside createHCSTopic which don't have getters
-        assertEquals(1, getTopicInfo.getOverrideNodeMap().size());
-        assertEquals("testing.hedera.com", getTopicInfo.getOverrideNodeMap().get(AccountId.fromString("0.0.19")));
-        assertEquals("0.0.10", getTopicInfo.getOverrideOperatorAccountId().toString());
-        assertEquals(ed25519PrivateKey.toString(), getTopicInfo.getOverrideOperatorKey().toString());
+        assertEquals(1, getTopicInfo.getNodeMap().size());
+        assertEquals("testing.hedera.com", getTopicInfo.getNodeMap().get(AccountId.fromString("0.0.19")));
+        assertEquals("0.0.10", getTopicInfo.getOperatorAccountId().toString());
+        assertEquals(ed25519PrivateKey.toString(), getTopicInfo.getOperatorKey().toString());
     }
 }

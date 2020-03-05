bf3155422210975c50111d1f1b6b9525fe50ea97
Greg Scullard <gregscullard@hedera.com>
2020-03-05 14:25:12 +0100

    Generate pair-wise from docker file instead of template
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hcs-sxc-java-examples/hcs-sxc-java-settlement-demo/src/main/java/com/hedera/hcsapp/GenerateConfigurationFiles.java
+++ hcs-sxc-java-examples/hcs-sxc-java-settlement-demo/src/main/java/com/hedera/hcsapp/GenerateConfigurationFiles.java
@@ -20,12 +20,10 @@ package com.hedera.hcsapp;
  * ‍
  */
 
-import com.google.common.collect.HashBiMap;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
 import com.hedera.hcs.sxc.plugin.cryptography.StringUtils;
 import com.hedera.hcs.sxc.plugin.cryptography.cryptography.Cryptography;
 import java.io.File;
-
 
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -33,92 +31,102 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
+
 public class GenerateConfigurationFiles {
 
     public static void main(String[] args) throws Exception {
-        
+
         // load the composer template that contains all participants
         Yaml dockerComposeYaml = new Yaml();
-        File dockerComposeInput = new File("./config/docker-compose.yml.sample");
+        String dockerComposeFile = new File("./config/docker-compose.yml").getCanonicalPath();
+        File dockerComposeInput = new File(dockerComposeFile);
         InputStream dockerComposeInputStream = new FileInputStream(dockerComposeInput);
-        Map<String,
-           Map<String,
-             Map<String,
-                Map<String,String>
-        >>> dockComposeTemplate = dockerComposeYaml.load(dockerComposeInputStream);
-        
-        // loast all address list template that contains relationships between participants
-        Yaml addressYaml = new Yaml();
-        File addressInput = new File("./config/contact-list.template.yaml");
-        InputStream addressInputStream = new FileInputStream(addressInput);
-        Map<String,
-           Map<String,
-             Map<String,String>
-        >> addressTemplate = addressYaml.load(addressInputStream);
-        
-        
-       
-        for (String player :  dockComposeTemplate.get("services").keySet()){
-            Map<String, String> env = dockComposeTemplate
-                    .get("services")
-                    .get(player)
-                    .get("environment");
-                    Ed25519PrivateKey key = Ed25519PrivateKey.generate();            
-                    env.put("PUBKEY", key.publicKey.toString());
-                    env.put("SIGNKEY", key.toString());
-                    
-                    // pobulate all public keys and init mutable map
-                    for (String playerInAddress : addressTemplate.keySet()){
-                        if (addressTemplate.get(playerInAddress).containsKey(player)){
-                            if (addressTemplate.get(playerInAddress).get(player) == null){
-                                Map<String,String> m = new HashMap<>();
-                                m.put("theirEd25519PubKeyForSigning", key.publicKey.toString());
-                                addressTemplate.get(playerInAddress)
-                                        .put(player, m)
-                                ;
-                            } else {
-                                addressTemplate.get(playerInAddress)
-                                .get(player).put(
-                                        "theirEd25519PubKeyForSigning", key.publicKey.toString()
-                                ) ;
+        Map<String, Map<String, Map<String, Map<String, String>>>> dockComposeTemplate = dockerComposeYaml
+                .load(dockerComposeInputStream);
+
+        Map<String, Map<String, Map<String, String>>> addressTemplate = new HashMap<String, Map<String, Map<String, String>>>();
+
+        for (String appUser : dockComposeTemplate.get("services").keySet()) {
+            String appUserRoles = dockComposeTemplate.get("services").get(appUser).get("environment").get("ROLES");
+
+            for (String otherParty : dockComposeTemplate.get("services").keySet()) {
+                String otherPartyRoles = dockComposeTemplate.get("services").get(otherParty).get("environment")
+                        .get("ROLES");
+
+                if (!appUser.contentEquals(otherParty)) {
+                    switch (appUserRoles) {
+                    case "PAYCHANNEL":
+                        // paychannels exchange with everyone except pay channels
+                        if (!otherPartyRoles.contentEquals("PAYCHANNEL")) {
+                            if (addressTemplate.get(appUser) == null) {
+                                addressTemplate.put(appUser,new HashMap<String, Map<String, String>>());
                             }
+                            addressTemplate.get(appUser).put(otherParty, new HashMap<String, String>());
                         }
-                     }
-                    
-                     // set pair wise shared keys
-                    for (String playerInAddress : addressTemplate.keySet()){
-                        if (addressTemplate.get(playerInAddress).containsKey(player)){
-                            //if (addressTemplate.get(playerInAddress).get(player) == null){
-                                //Map<String, String> p = addressTemplate.get(playerInAddress).get(player);
-                                String sharedKey = StringUtils.byteArrayToHexString( Cryptography.generateRsaKeyPair().getPrivate().getEncoded());
-                                //Map<String,String> m2 = new HashMap<>();
-                                
-                                addressTemplate.get(playerInAddress)
-                                        .get(player)
-                                        .put("sharedSymmetricEncryptionKey", sharedKey);
-                                //Map<String,String> m3 = new HashMap<>();
-                                if(addressTemplate.get(player)
-                                        .get(playerInAddress) !=null ){
-                                addressTemplate.get(player)
-                                        .get(playerInAddress)
-                                        .put("sharedSymmetricEncryptionKey", sharedKey)
-                                ;
-                                }
-                            }    
-                        //}   
-                        
-                   
+                        break;
+                    case "BUYER,SELLER":
+                        // buyer/seller exchange with everyone
+                    case "DEMO":
+                        // Demo receive from everyone
+                    case "ARBITER,AUDITOR":
+                        // Arbiter/auditor receive from everyone
+                        if (addressTemplate.get(appUser) == null) {
+                            addressTemplate.put(appUser,new HashMap<String, Map<String, String>>());
+                        }
+                        addressTemplate.get(appUser).put(otherParty, new HashMap<String, String>());
+                        break;
                     }
-                    
+                }
+            }
+        }
+
+        for (String player : dockComposeTemplate.get("services").keySet()) {
+            Map<String, String> env = dockComposeTemplate.get("services").get(player).get("environment");
+            Ed25519PrivateKey key = Ed25519PrivateKey.generate();
+            env.put("PUBKEY", key.publicKey.toString());
+            env.put("SIGNKEY", key.toString());
+
+            // populate all public keys and init mutable map
+            for (String playerInAddress : addressTemplate.keySet()) {
+                if (addressTemplate.get(playerInAddress).containsKey(player)) {
+                    if (addressTemplate.get(playerInAddress).get(player) == null) {
+                        Map<String, String> m = new HashMap<>();
+                        m.put("theirEd25519PubKeyForSigning", key.publicKey.toString());
+                        addressTemplate.get(playerInAddress).put(player, m);
+                    } else {
+                        addressTemplate.get(playerInAddress).get(player).put("theirEd25519PubKeyForSigning",
+                                key.publicKey.toString());
+                    }
+                }
+            }
+
+            // set pair wise shared keys
+            for (String playerInAddress : addressTemplate.keySet()) {
+                if (addressTemplate.get(playerInAddress).containsKey(player)) {
+                    String sharedKey = StringUtils
+                            .byteArrayToHexString(Cryptography.generateRsaKeyPair().getPrivate().getEncoded());
+
+                    addressTemplate.get(playerInAddress).get(player).put("sharedSymmetricEncryptionKey", sharedKey);
+                    if (addressTemplate.get(player) != null) {
+                        if (addressTemplate.get(player).get(playerInAddress) != null) {
+                            addressTemplate.get(player).get(playerInAddress).put("sharedSymmetricEncryptionKey",
+                                    sharedKey);
+                        }
+                    }
+                }
+            }
         }
         Yaml dockComposeOut = new Yaml();
-        FileWriter dockComposeWriter = new FileWriter("./config/docker-compose.yml");
+        FileWriter dockComposeWriter = new FileWriter(dockerComposeFile);
         dockComposeWriter.write(dockComposeOut.dumpAsMap(dockComposeTemplate));
         dockComposeWriter.close();
-                
+
         Yaml addressOut = new Yaml();
-        FileWriter addressOutWriter = new FileWriter("./config/contact-list.yaml");
+        String contactListFile = new File("./config/contact-list.yaml").getCanonicalPath();
+        FileWriter addressOutWriter = new FileWriter(contactListFile);
         addressOutWriter.write(dockComposeOut.dumpAsMap(addressTemplate));
-        addressOutWriter.close(); 
+        addressOutWriter.close();
+
+        System.out.println("Configuration file generation complete.");
     }
 }

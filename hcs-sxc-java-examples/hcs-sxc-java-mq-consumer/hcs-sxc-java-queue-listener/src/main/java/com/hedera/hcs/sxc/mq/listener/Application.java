5aa29401dc636f6fe07bba739f660118827feb12
Greg Scullard <gregscullard@hedera.com>
2020-04-14 16:25:25 +0200

    tidy up
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hcs-sxc-java-examples/hcs-sxc-java-mq-consumer/hcs-sxc-java-queue-listener/src/main/java/com/hedera/hcs/sxc/mq/listener/Application.java
+++ hcs-sxc-java-examples/hcs-sxc-java-mq-consumer/hcs-sxc-java-queue-listener/src/main/java/com/hedera/hcs/sxc/mq/listener/Application.java
@@ -20,11 +20,9 @@ package com.hedera.hcs.sxc.mq.listener;
  * ‍
  */
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Log4j2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

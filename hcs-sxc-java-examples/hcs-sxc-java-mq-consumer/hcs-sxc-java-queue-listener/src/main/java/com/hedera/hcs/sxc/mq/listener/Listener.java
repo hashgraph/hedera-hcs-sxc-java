a90de334489054246ffc9a2fc20b1375fc51e0e0
Greg Scullard <gregscullard@hedera.com>
2020-04-14 16:25:49 +0200

    Allow for optional setting of `producer`
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hcs-sxc-java-examples/hcs-sxc-java-mq-consumer/hcs-sxc-java-queue-listener/src/main/java/com/hedera/hcs/sxc/mq/listener/Listener.java
+++ hcs-sxc-java-examples/hcs-sxc-java-mq-consumer/hcs-sxc-java-queue-listener/src/main/java/com/hedera/hcs/sxc/mq/listener/Listener.java
@@ -12,8 +12,6 @@ import com.hedera.hcs.sxc.mq.listener.config.AppData;
 import com.hedera.hcs.sxc.mq.listener.config.Config;
 import com.hedera.hcs.sxc.mq.listener.config.Queue;
 import com.hedera.hcs.sxc.proto.ApplicationMessage;
-import com.hedera.hcs.sxc.signing.Signing;
-import com.hedera.hcs.sxc.utils.StringUtils;
 import com.rabbitmq.client.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
@@ -21,70 +19,60 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 @Log4j2
 @Component
 public class Listener {
-    private String messageThread = ""; // used to group messages by an conversational id.
-    private Map<String, List<String>> messageThreads = new HashMap<String, List<String>>();
-    private int topicIndex = 0; // refers to the first topic ID in the config.yaml
-
     @PostConstruct
     public void init() throws Exception {
 
-        String appId = "RabbitMQ-Listener";
-
-      
-        
-        
         Queue queueConfig = null;
         try {
             queueConfig = new Config().getConfig().getQueue();
         } catch (IOException ex) {
             log.error(ex);
         }
-        
+
         final Queue queueConfigFinal = queueConfig;
-        
+
         // create a callback object to receive the message
         OnHCSMessageCallback onHCSMessageCallback = new OnHCSMessageCallback(AppData.getHCSCore());
         onHCSMessageCallback.addObserver((SxcConsensusMessage sxcConsensusMesssage, HCSResponse hcsResponse) -> {
             // handle notification in mirrorNotification
-            System.out.println("got hcs response - feeding back to out queue");
-            //printVerboseDetails(hcsCore, hcsResponse);
-            
-             ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(queueConfigFinal.getHost());
-            factory.setUsername(queueConfigFinal.getUser());
-            factory.setPassword(queueConfigFinal.getPassword());
-            //factory.setPort(queueConfigFinal.getPort());
+            if (queueConfigFinal.getProducerTag() == null) {
+                System.out.println("got hcs response");
+                try {
+                    System.out.println(" [x] Received '" + queueConfigFinal.getConsumerTag() + "':'"
+                            + getSimpleDetails(AppData.getHCSCore(), hcsResponse) + "'");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("got hcs response - feeding back to out queue");
 
-            try (Connection connection = factory.newConnection();
-                   Channel channel = connection.createChannel()) {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.setHost(queueConfigFinal.getHost());
+                factory.setUsername(queueConfigFinal.getUser());
+                factory.setPassword(queueConfigFinal.getPassword());
 
-                   channel.exchangeDeclare(queueConfigFinal.getExchangeName(), "topic");
-
-                   //String routingKey = getRouting(argv);
-                   //String message = getMessage(argv);
-
-                   channel.basicPublish(queueConfigFinal.getExchangeName(), queueConfigFinal.getProducerTag() , null, getSimpleDetails(AppData.getHCSCore(), hcsResponse).getBytes());
-                   System.out.println(" [x] Sent '" + queueConfigFinal.getProducerTag() + "':'" + getSimpleDetails(AppData.getHCSCore(), hcsResponse) + "'");
-            } catch (IOException ex) {
-                log.error(ex);
-            } catch (TimeoutException ex) {
-                 log.error(ex);
-            } catch (Exception ex) {
-                 log.error(ex);
+                try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+                    channel.exchangeDeclare(queueConfigFinal.getExchangeName(), "topic");
+                    channel.basicPublish(queueConfigFinal.getExchangeName(), queueConfigFinal.getProducerTag(), null,
+                            getSimpleDetails(AppData.getHCSCore(), hcsResponse).getBytes());
+                    System.out.println(" [x] Sent '" + queueConfigFinal.getProducerTag() + "':'"
+                            + getSimpleDetails(AppData.getHCSCore(), hcsResponse) + "'");
+                } catch (IOException ex) {
+                    log.error(ex);
+                } catch (TimeoutException ex) {
+                    log.error(ex);
+                } catch (Exception ex) {
+                    log.error(ex);
+                }
             }
         });
 
-
         System.out.println("Loaded APP_ID:" + AppData.getHCSCore().getApplicationId());
-
 
         Runnable runnable;
         runnable = () -> {
@@ -94,31 +82,25 @@ public class Listener {
             factory.setPassword("guest");
             factory.setPort(5672);
 
-            try (
-                    Connection connection = factory.newConnection();
-                    Channel channel = connection.createChannel()) {
-                //channel.queueDeclare(queueConfig.getName(), false, false, false, null);//(queueConfig.getName(), false, false, false, null);
+            try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
                 channel.exchangeDeclare("testExchange", "topic");
-                String consumerTag ="myConsumerTag";
+                String consumerTag = "myConsumerTag";
                 String queueName = channel.queueDeclare().getQueue();
                 channel.queueBind(queueName, "testExchange", consumerTag);
                 System.out.println("Connected");
-                boolean autoAck = false;
-                
-                
+
                 DeliverCallback deliverCallback = (consumerTagPrime, delivery) -> {
                     String message = new String(delivery.getBody(), "UTF-8");
-                    System.out.println(" [x] Received '" +
-                            delivery.getEnvelope().getRoutingKey() + "':'" + message + "'" + "on "+consumerTagPrime);
+                    System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message
+                            + "'" + "on " + consumerTagPrime);
                     try {
-                            OutboundHCSMessage outboundHCSMessage
-                                    = new OutboundHCSMessage(AppData.getHCSCore());
-                            outboundHCSMessage.sendMessage(0, message.getBytes());
-                        } catch (Exception ex) {
-                            log.error(ex);
-                        }
+                        OutboundHCSMessage outboundHCSMessage = new OutboundHCSMessage(AppData.getHCSCore());
+                        outboundHCSMessage.sendMessage(0, message.getBytes());
+                    } catch (Exception ex) {
+                        log.error(ex);
+                    }
                 };
-                channel.basicConsume(queueName, true, deliverCallback, consumerTagPrime -> { 
+                channel.basicConsume(queueName, true, deliverCallback, consumerTagPrime -> {
                     System.out.println("woot");
                 });
 
@@ -133,88 +115,24 @@ public class Listener {
         Thread thread = new Thread(runnable);
         thread.start();
     }
-    
+
     private static String getSimpleDetails(HCSCore hcsCore, HCSResponse hcsResponse) {
-        String ret=null;
+        String ret = null;
         try {
-            SxcApplicationMessageInterface applicationMessageEntity
-                    = hcsCore
-                            .getPersistence()
-                            .getApplicationMessageEntity(
-                                    SxcPersistence.extractApplicationMessageStringId(
-                                            hcsResponse.getApplicationMessageId()
-                                    )
-                            );
-            ret =  "S:"+applicationMessageEntity.getLastChronoPartSequenceNum()+
-                    "T:"+applicationMessageEntity.getLastChronoPartConsensusTimestamp()+
-                    "M:"+
-                    ApplicationMessage.parseFrom(applicationMessageEntity.getApplicationMessage())
+            SxcApplicationMessageInterface applicationMessageEntity = hcsCore.getPersistence()
+                    .getApplicationMessageEntity(
+                            SxcPersistence.extractApplicationMessageStringId(hcsResponse.getApplicationMessageId()));
+            ret = "S:" + applicationMessageEntity.getLastChronoPartSequenceNum() + "T:"
+                    + applicationMessageEntity.getLastChronoPartConsensusTimestamp() + "M:"
+                    + ApplicationMessage.parseFrom(applicationMessageEntity.getApplicationMessage())
                             .getBusinessProcessMessage().toString("UTF-8");
             ;
-            
-            
-            
+
         } catch (UnsupportedEncodingException ex) {
             log.error(ex);
         } catch (InvalidProtocolBufferException ex) {
             log.error(ex);
         }
         return ret;
-    }
-    
-
-    private static void printVerboseDetails(HCSCore hcsCore, HCSResponse hcsResponse) {
-        try {
-            SxcApplicationMessageInterface applicationMessageEntity
-                    = hcsCore
-                            .getPersistence()
-                            .getApplicationMessageEntity(
-                                    SxcPersistence.extractApplicationMessageStringId(
-                                            hcsResponse.getApplicationMessageId()
-                                    )
-                            );
-            System.out.println("Details stored as applicationMessageEntity : ");
-            System.out.printf("    applicationMessageId: %s \n", applicationMessageEntity.getApplicationMessageId());
-            System.out.printf("    last chrono chunk consensus sequenceNum: %s \n", applicationMessageEntity.getLastChronoPartSequenceNum());
-            System.out.printf("    last chrono chunk consensus running hash: %s \n", applicationMessageEntity.getLastChronoPartRunningHashHEX());
-            System.out.printf("    last chrono chunk consensus timestamp: %s \n", applicationMessageEntity.getLastChronoPartConsensusTimestamp());
-            
-            System.out.println("    ApplicationMessage: ");
-            ApplicationMessage appMessage = ApplicationMessage.parseFrom(applicationMessageEntity.getApplicationMessage());
-            System.out.printf("        Id: %s \n", SxcPersistence.extractApplicationMessageStringId(appMessage.getApplicationMessageId()));
-            System.out.printf("        Hash of unencrypted message: %s \n",
-                    StringUtils.byteArrayToHexString(
-                            appMessage.getUnencryptedBusinessProcessMessageHash().toByteArray()
-                    )
-            );
-            System.out.printf("        Signature on hash above: %s \n",
-                    StringUtils.byteArrayToHexString(
-                            appMessage.getBusinessProcessSignatureOnHash().toByteArray()
-                    )
-            );
-
-            byte[] bpm = appMessage.getBusinessProcessMessage().toByteArray();
-            System.out.printf("        BPM: %s \n",
-                    new String(bpm)
-            );
-
-            System.out.printf("        Encryption random: %s \n",
-                    StringUtils.byteArrayToHexString(
-                            appMessage.getEncryptionRandom().toByteArray()
-                    )
-            );
-
-            System.out.printf("        Is this an echo?: %s \n",
-                    Signing.verify(
-                            appMessage.getUnencryptedBusinessProcessMessageHash().toByteArray(),
-                            appMessage.getBusinessProcessSignatureOnHash().toByteArray(),
-                            hcsCore.getMessageSigningKey().publicKey
-                    )
-            );
-
-        } catch (InvalidProtocolBufferException ex) {
-            log.error(ex.getStackTrace());
-        }
-
     }
 }
